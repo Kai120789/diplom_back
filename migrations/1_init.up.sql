0f97d8acb9fc2650c6fe803ad40a694b065d5843
Kai120789 <zoomer120789@gmail.com>
2025-01-13 22:08:22 +0300

    feat: added migrations for users table

---
+++ migrations/1_init.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(25) UNIQUE NOT NULL,
+    password VARCHAR(60) NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW (),
+    updated_at TIMESTAMPTZ DEFAULT NOW ()
+);
